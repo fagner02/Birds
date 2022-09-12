31a01949db627e41a5e6c827dd2613dfd1e894c6
Fagner <fagnerrc12@gmail.com>
2022-09-12 00:36:31 -0300

    Update direction calc

--- Scene.cs
+++ Scene.cs
@@ -11,7 +11,7 @@ public class Scene : Spatial
 
     public override void _Ready()
     {
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < 1000; i++)
         {
             var bird = (Node2D)GD.Load<PackedScene>("res://Bird.tscn").Instance();
             AddChild(bird);
@@ -24,10 +24,12 @@ public class Scene : Spatial
     public override void _Process(float delta)
     {
         delta *= 100 * 5;
+        var last = 0.0;
         for (int i = 0; i < birds.Count; i++)
         {
             var screenSize = GetViewport().Size;
-            nudges[i] += (rand.NextDouble() > 0.5 ? 1 : -1) * rand.NextDouble() * 1.5;
+            var limit = (nudges[(nudges.Count + i - 1) / nudges.Count] + 2) / 4;
+            nudges[i] += (rand.NextDouble() < limit ? 1 : -1) * rand.NextDouble() * 1.5;
             if (nudges[i] > 1)
             {
                 nudges[i] = 1;
@@ -38,27 +40,30 @@ public class Scene : Spatial
             }
 
             var update = true;
-            directions[i] += (nudges[i] * (((1 - Math.Abs(nudges[i])))));
-            directions[i] = directions[i] % (Math.PI * 2);
-            if (birds[i].Position.x - 1 < 1 || birds[i].Position.x + 1 > screenSize.x - 1)
-            {
-                directions[i] = directions[i] < Math.PI ? Math.PI - directions[i] : Math.PI * 3 - directions[i];
-                update = false;
-            }
-            if (birds[i].Position.y - 1 < 1 || birds[i].Position.y + 1 > screenSize.y - 1)
-            {
-                directions[i] = Math.PI * 2 - directions[i];
-                update = false;
-            }
-
-
+            directions[i] += nudges[i] * Math.Abs(nudges[i]) / 20;
             directions[i] = directions[i] % (Math.PI * 2);
             var y = (float)Math.Sin(directions[i]);
             var x = (float)Math.Cos(directions[i]);
+            if (birds[i].Position.x + x < 0 || birds[i].Position.x + x > screenSize.x)
+            {
+                // directions[i] = directions[i] < Math.PI ? Math.PI - directions[i] : Math.PI * 3 - directions[i];
+                birds[i].Position = new Vector2(screenSize.x - birds[i].Position.x, birds[i].Position.y);
+                update = false;
+            }
+            if (birds[i].Position.y + y < 0 || birds[i].Position.y + y > screenSize.y)
+            {
+                // directions[i] = Math.PI * 2 - directions[i];
+                birds[i].Position = new Vector2(birds[i].Position.x, screenSize.y - birds[i].Position.y);
+                update = false;
+            }
+
+
+            directions[i] = directions[i] % (Math.PI * 2);
+
             if (!update)
             {
-                // y *= 0;
-                // x *= 0;
+                y *= 0;
+                x *= 0;
             }
 
             birds[i].Translate(new Vector2(x, y) * delta);
