55bf65298ee29998c7c9bd26783e54baa5aa9d8a
Fagner Ferreira <fagnerrc12@gmail.com>
2025-06-09 05:52:06 -0300

    update

--- Scene.cs
+++ Scene.cs
@@ -1,13 +1,15 @@
-using Godot;
 using System;
 using System.Collections.Generic;
+using Godot;
+
+namespace Birds;
 
 public partial class Scene : Node3D
 {
-	private List<Node2D> birds = new List<Node2D>();
-	private List<double> directions = new List<double>();
-	private List<double> nudges = new List<double>();
-	private readonly Random rand = new Random();
+	private readonly List<Node2D> birds = [];
+	private readonly List<double> directions = [];
+	private readonly List<double> nudges = [];
+	private readonly Random rand = new();
 
 	public override void _Ready()
 	{
@@ -16,66 +18,63 @@ public partial class Scene : Node3D
 			var bird = (Node2D)GD.Load<PackedScene>("res://Bird.tscn").Instantiate();
 			AddChild(bird);
 			birds.Add(bird);
-			directions.Add(0);
-			nudges.Add(0);
+			directions.Add(rand.NextDouble() * Math.PI * 2.0);
+			nudges.Add(rand.NextDouble() * Math.PI * 2.0);
 		}
 	}
 
 	public override void _Process(double delta)
 	{
-		// return;
-		delta *= 100 * 5;
-		var last = 0.0;
+		Vector2 mouse = GetViewport().GetMousePosition();
+		Vector2 screenSize = GetViewport().GetVisibleRect().Size;
 		for (int i = 0; i < birds.Count; i++)
 		{
-			var screenSize = GetViewport().GetVisibleRect().Size;
-			var limit = (nudges[(nudges.Count + i - 1) / nudges.Count] + 2) / 4;
-			nudges[i] += (rand.NextDouble() < limit ? 1 : -1) * rand.NextDouble() * 1.5;
-			if (nudges[i] > 1)
-			{
-				nudges[i] = 1;
-			}
-			if (nudges[i] < -1)
-			{
-				nudges[i] = -1;
-			}
+			Vector2 pos = birds[i].GlobalPosition;
+			double angle = (pos - mouse).Angle();
+			double diff = Math.Abs(angle - directions[i]);
+			double diff2 = (Math.PI * 2) - diff;
 
-			var update = true;
-			directions[i] += nudges[i] * Math.Abs(nudges[i]) / 20;
-			directions[i] = directions[i] % (Math.PI * 2);
-			var y = (float)Math.Sin(directions[i]);
-			var x = (float)Math.Cos(directions[i]);
+			double nudge;
+			if (diff2 < diff)
+				nudge = -diff2;
+			else
+				nudge = diff;
+
+			if (angle > directions[i])
+				nudge *= -1;
+
+			nudges[i] += nudge * 0.1;
+			if (nudges[i] > 1)
+				nudges[i] = 1;
+			if (nudges[i] < -1)
+				nudges[i] = -1;
+
+
+			directions[i] += nudges[i] * delta * 5;
+			if (Math.Abs(directions[i]) > Math.PI * 2)
+				directions[i] = directions[i] * -1 % Math.PI * 2;
+
+			float y = (float)Math.Sin(directions[i]);
+			float x = (float)Math.Cos(directions[i]);
+			Console.WriteLine($"{diff2}, {diff}, {directions[i]}, ({x},{y})");
 			if (birds[i].Position.X + x < 0 || birds[i].Position.X + x > screenSize.X)
 			{
-				directions[i] = directions[i] < Math.PI ? Math.PI - directions[i] : Math.PI * 3 - directions[i];
-				birds[i].Position = new Vector2(screenSize.X - birds[i].Position.X, birds[i].Position.Y);
-				update = false;
+				birds[i].Position = new Vector2(
+					screenSize.X - Math.Clamp(birds[i].Position.X, 0, screenSize.X),
+					birds[i].Position.Y
+				);
 			}
 			if (birds[i].Position.Y + y < 0 || birds[i].Position.Y + y > screenSize.Y)
 			{
-				directions[i] = Math.PI * 2 - directions[i];
-				birds[i].Position = new Vector2(birds[i].Position.X, screenSize.Y - birds[i].Position.Y);
-				update = false;
+				birds[i].Position = new Vector2(
+					birds[i].Position.X,
+					screenSize.Y - Math.Clamp(birds[i].Position.Y, 0, screenSize.Y)
+				);
 			}
 
+			birds[i].Translate(new Vector2(x, y) * (float)delta * 500);
 
-			directions[i] = directions[i] % (Math.PI * 2);
-
-			if (!update)
-			{
-				y *= 0;
-				x *= 0;
-			}
-
-			birds[i].Translate(new Vector2(x, y) * (float)delta);
-
-			var rotation = birds[i].Rotation;
-			if (rotation < directions[i] - 0.1 || rotation > directions[i] + 0.1)
-			{
-				birds[i].Rotate((float)delta * (rotation > directions[i] ? -.01f : 0.01f));
-				((Node2D)birds[i].GetChild(0)).Rotation = ((float)delta * (rotation > directions[i] ? -0.26f : 0.26f));
-				((Node2D)birds[i].GetChild(1)).Rotation = ((float)delta * (rotation > directions[i] ? -0.26f : 0.26f));
-			}
+			birds[i].Rotation = (float)(1 * directions[i]);
 		}
 	}
 }
